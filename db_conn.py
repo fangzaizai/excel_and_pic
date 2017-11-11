971ba2182010d78f5ac8240e8156616043be08c7
fangzaizai <anmeifang111@sina.com>
2017-11-11 18:02:46 +0800

    config
    q

--- db_conn.py
+++ db_conn.py
@@ -2,14 +2,15 @@
 import psycopg2, os
 import ConfigParser
 
-def db_conn():
+def ConParser():
 	conf=ConfigParser.ConfigParser()
 	conf.read('config.ini')
 	server=conf.get('database','server')
 	dbname=conf.get('database','dbname')
 	username=conf.get('database','username')
 	password=conf.get('database','password')
-	print server
+
+def db_conn():
 	conn = psycopg2.connect(database=dbname, user=username, password=password, host=server, port='5432')
 	return conn
 
@@ -30,13 +31,11 @@ INNER JOIN face_feature_info AS B
 ON A .person_id = B.person_id)
 INNER JOIN face_attri_result AS C
 on A.fr_alarm_id = C.recordid)
-where A.fr_alarmtime between '2017-11-10 16:55:00' and '2017-11-10 17:00:00'
+where A.fr_alarmtime between '2017-11-10 14:00:00' and '2017-11-10 16:00:00'
+order by A.fr_alarmtime desc
 '''
 	cur.execute(sql)
 	result=cur.fetchall()
-	#file = open('1.txt','wb')
-	#file.write(str(result))
-	#file.close()
 	print result[0][0]
 	return result
 
