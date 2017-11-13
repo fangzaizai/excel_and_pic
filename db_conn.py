dcc7b5a68964710bd014f89876f75be659b4ee92
fangzaizai <anmeifang111@sina.com>
2017-11-13 17:23:49 +0800

    wobudongl

--- db_conn.py
+++ db_conn.py
@@ -22,7 +22,10 @@ def ConParser():
 
 
 paras=ConParser()
+
+print paras['dbname']
 def db_conn():
+	paras=ConParser()
 	print paras['db_server']
 	#print paras['Starttime']
 	conn = psycopg2.connect(database=paras['dbname'], user=paras['db_username'], password=paras['db_password'], host=paras['db_server'], port='5432')
@@ -32,33 +35,23 @@ def db_close(conn):
 	conn.close()
 
 def db_query(conn):
+	paras=ConParser()
 	#Start_time=datetime.datetime.strptime(paras['Starttime'],"%Y-%m-%d %H:%M:%S")
 	#End_time=datetime.datetime.strptime(paras['Endtime'],"%Y-%m-%d %H:%M:%S")
 	cur=conn.cursor()
-	sql = 'SELECT\
-	A.fr_alarmtime,\
-	A.fr_alarm_cameraname,\
-	A.fr_alarm_similarity,\
-	A .person_name,\
-	B.photo_path,\
-	C.imagepath1 \
-FROM ((face_recognize_alarm AS A \
-INNER JOIN face_feature_info AS B \
-ON A .person_id = B.person_id) \
-INNER JOIN face_attri_result AS C \
-on A.fr_alarm_id = C.recordid) \
-where A.fr_alarmtime between '+paras['Starttime']+' and '+paras['Endtime']+ ' order by A.fr_alarmtime desc'
+	sql_header = "SELECT A.fr_alarmtime,A.fr_alarm_cameraname,A.fr_alarm_similarity,A .person_name,C.imagepath1,B.photo_path FROM ((face_recognize_alarm AS A JOIN face_feature_info AS B ON A .person_id = B.person_id) INNER JOIN face_attri_result AS C on A.fr_alarm_id = C.recordid) where A.fr_alarmtime between "
+	sql_tail=" order by A.fr_alarmtime desc"
+	sql=sql_header+str(paras['Starttime'])+' and '+str(paras['Endtime'])+ sql_tail
 	print sql
 	cur.execute(sql)
 	result=cur.fetchall()
-	#print result[0][0]
 	return result
 
 def ftp_open():
 	ftp=FTP()
 	ftp.set_debuglevel(2)
-	ftp.connect(paras.ftp_server,21)
-	ftp.login(paras.ftp_username,paras.ftp_password)
+	ftp.connect(paras['ftp_server'],21)
+	ftp.login(paras['ftp_username'],paras['ftp_password'])
 	return ftp
 
 def ftp_close(ftp):
@@ -67,7 +60,7 @@ def ftp_close(ftp):
 def ftp_get_image(ftp, row, col, pic_path):
 	pic_root_path=paras['pic_root_path']+ pic_path  #/LINDASceneAlarm/20171107/13/{1C59EA90-9963-4405-849B-200A66F39133}-20171107131802560.jpg'
 	buffersize=1024
-	local_pic=path+'\pic\\'+str(row)+str(col)+'.jpg'
+	local_pic=os.getcwd()+'\pic\\'+str(row)+str(col)+'.jpg'
 	try:
 		fp=open(local_pic,'wb')
 		ftp.retrbinary('RETR ' + pic_root_path, fp.write,buffersize)
--- insert_pic.py
+++ insert_pic.py
@@ -19,59 +19,7 @@ def open_xls(file):
 	except Exception,e:
 		print str(e)
  #formatting
-def data_byindex(file):
-	data=open_xls(file)
-	table=data.sheets()[0]
-	#copydata:<class 'xlwt.Workbook.Workbook'>
-	copydata=copy(data)
-	#tmpdata:<class 'xlwt.Worksheet.Worksheet'>
-	tmpdata=copydata.get_sheet(0)  
-	#get row nums
-	row_num=table.nrows    
-	print row_num
-	#get values
-	cellM0=table.cell(4,12).value
-	print cellM0
-	tmpdata.col(12).width = 0x0d00 + 6500
-	tmpdata.col(13).width = 0x0d00 + 6500
-	for i in range(2,row_num):
-		row_height=xlwt.easyxf('font:height 1800;')
-		tmpdata.row(i).set_style(row_height)
-	print table.col(3)[5]
-	#return table
-	copydata.save('2.xls')
-	return table
-'''
-def format_excel_pri(file):
-	data=open_xls(file)
-	table=data.sheets()[0]
-	#format1 = table.format()
-	nrows=table.nrows
-	ncols=table.ncols
-	#print type(table)
-	workbook = xlsxwriter.Workbook(r'报警记录统计.xls')
-	worksheet = workbook.add_worksheet()
-	worksheet.set_column(12,13, 30)
-	#worksheet.set_column(0,13, 80)
-	ftp=ftp_open('192.168.5.199')
-	for i in xrange(nrows):
-		worksheet.set_row(i,150)
-		for j in xrange(ncols):
-			cell_value=table.cell_value(i,j)
-			worksheet.write(i,j,cell_value)
-			if (j == 12 and i >3):
-				pic_path=ftp_get_image(ftp, i, 12, cell_value)
-				resize(pic_path)
-				worksheet.insert_image('M'+str(i), pic_path)
-				print 'insert pic'
-			elif (j==13 and i >3):
-				pic_path=ftp_get_image(ftp, i, 13, cell_value)
-				resize(pic_path)
-				worksheet.insert_image('N'+str(i), pic_path)
-				print 'insert another pic'			
-	ftp_close(ftp)
-	workbook.close()
-	'''
+
 def format_excel():
 	workbook = xlsxwriter.Workbook('alarm.xls')
 	date_format = workbook.add_format({'num_format':'yyyy-mm-dd hh:mm:ss','align':'center','valign':'vcenter'})
@@ -92,7 +40,7 @@ def format_excel():
 	print 'ftp conn over'
 	worksheet.write(0,0,u'报警记录统计') #此处需要一个merge
 	#worksheet.insert_image('F1', '104.jpg')
-	for i in range(0, ncols+1):  #针对每一行，依次填充列值
+	for i in range(1, ncols+1):  #针对每一行，依次填充列值
 		worksheet.set_row(i,150)
 		for j in xrange(6):
 			cell_value=result[i-1][j]
@@ -117,21 +65,7 @@ def resize(pic_path):
 	image_resized=image.resize((200,200),Image.ANTIALIAS)
 	image_resized.save(pic_path)
 	print 'resize pic'
-#can be delete
-def ftp_get_image(ftp, row, col, pic_path):
-	pic_root_path='/home/ubuntu/FTP/'+ pic_path  #/LINDASceneAlarm/20171107/13/{1C59EA90-9963-4405-849B-200A66F39133}-20171107131802560.jpg'
-	buffersize=1024
-	local_pic=path+'\pic\\'+str(row)+str(col)+'.jpg'
-	try:
-		fp=open(local_pic,'wb')
-		ftp.retrbinary('RETR ' + pic_root_path, fp.write,buffersize)
-	except IOError as ioerr:
-		print 'Error:%s' % (ioerr.errno)
-	ftp.set_debuglevel(0)
-	fp.close()
-	return local_pic
 
 
 if __name__ == '__main__':
 	format_excel()
-	#ftp_get_image(12,3,'/LINDASceneAlarm/20171107/13/{1C59EA90-9963-4405-849B-200A66F39133}-20171107132929015.jpg')
